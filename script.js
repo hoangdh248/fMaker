048681be8edc4e8b8327e671b06d383c49ee73fa
hoangdh248 <danghuyhoang24081998@gmail.com>
2020-12-25 00:53:32 +0700

    update code

--- script.js
+++ script.js
@@ -3,40 +3,54 @@
 let url = "wss://stream.binance.com:9443/ws/btcusdt@trade"
 let webSocketInstance = new WebSocket(url);
 let dataSource = []
-var lineChartInstance = null
+var otherChartInstance = null
+var priceChartInstance = null
 let intervalInstance = null
+let width = 800
+let speedPriceOnMinute = 0
+let beginTime
+let beginPrice
+Date.prototype.addMinutes = function (minutes) {
+    var copiedDate = new Date(this.getTime());
+    return new Date(copiedDate.getTime() + minutes * 60000);
+}
 const func = () => {
     let data = [...dataSource]
     dataSource = dataSource.slice(data.length - 1)
-    renderChart(lineChartInstance, data)
+    renderChart(otherChartInstance, priceChartInstance, data)
 }
 let flag = -1
 webSocketInstance.onmessage = function (event) {
     dataSource.push(event.data)
-    if(flag < 0){
+
+    if (flag < 0) {
+        let objectKeys = Object.keys(dataSource[0])
+        beginTime = new Date(dataSource[0][objectKeys[8]])
+        beginPrice = +JSON.parse(dataSource[0]).p
         func()
     }
     flag = 0
 }
-function renderChart(lineChart, dataSrc) {
+function renderChart(otherChart, priceChart, dataSrc) {
     let labels = []
+    let priceLabels = []
     let datasets = [{
-        label: "Price Avg",
-        data: [],
-        fill: false,
-        borderColor: "blue"
-    }, {
         label: "Amount Trade",
         data: [],
         fill: false,
-        borderColor: "red"
+        borderColor: "orange"
     }, {
         label: "Quantity Avg",
         data: [],
         fill: false,
-        borderColor: "green"
+        borderColor: "purple"
     }]
-
+    let priceDatasets = [{
+        label: "Price Avg",
+        data: [],
+        fill: false,
+        borderColor: "blue"
+    }]
 
 
 
@@ -44,64 +58,71 @@ function renderChart(lineChart, dataSrc) {
     let currentQuantityCalculate = []
     let amountTrade = 0
 
-    if(dataSrc.length > 0){
-        dataSrc.forEach(e => {
+    if (dataSrc.length > 0) {
+
+        dataSrc.forEach((e, i) => {
             e = JSON.parse(e)
             currentPricesCalculate.push(+e["p"])
             currentQuantityCalculate.push(+e["q"])
             amountTrade = amountTrade + 1
+
         });
-    
+
         let sumPrice = 0;
         currentPricesCalculate.forEach(price => {
             sumPrice += price;
         });
         let avgPrice = sumPrice / (currentPricesCalculate.length);
-    
-    
-    
-    
+
+
+
+
         let sumQuantity = 0;
         currentQuantityCalculate.forEach(q => {
             sumQuantity += q;
         });
         let avgQuantity = sumQuantity / (currentQuantityCalculate.length);
-    
-        if(typeof dataSrc[0] === "string"){
+
+        if (typeof dataSrc[0] === "string") {
             dataSrc[0] = JSON.parse(dataSrc[0])
         }
-        if(typeof dataSrc[dataSrc.length - 1] === "string"){
+        if (typeof dataSrc[dataSrc.length - 1] === "string") {
             dataSrc[dataSrc.length - 1] = JSON.parse(dataSrc[dataSrc.length - 1])
         }
-       let objectKeys = Object.keys(dataSrc[0]) 
-      
+        let objectKeys = Object.keys(dataSrc[0])
         let fromDateTime = new Date(dataSrc[0][objectKeys[8]]).toLocaleString("en-GB")
         let toDateTime = new Date(dataSrc[dataSrc.length - 1][objectKeys[8]]).toLocaleString("en-GB")
-    
-    
-    
-    
-    
-    
-    
-    
-        if (lineChart === null) {
+
+
+
+
+
+
+
+
+        if (!otherChart || !priceChart) {
             labels.push(fromDateTime + " – " + toDateTime)
-            datasets[0].data.push(avgPrice)
-            datasets[2].data.push(avgQuantity)
-            datasets[1].data.push(amountTrade)
-            let speedCanvas = document.getElementById("speedChart");
-    
-    
-    
+            priceLabels.push(fromDateTime + " – " + toDateTime)
+            priceDatasets[0].data.push(avgPrice)
+            datasets[1].data.push(avgQuantity)
+            datasets[0].data.push(amountTrade)
+            let otherCanvas = document.getElementById("otherChart");
+            let priceCanvas = document.getElementById("priceChart");
+
+
+
             Chart.defaults.global.defaultFontFamily = "Lato";
             Chart.defaults.global.defaultFontSize = 18;
             Chart.defaults.line.spanGaps = true;
-    
-    
-            let speedData = {
+
+
+            let otherData = {
                 labels,
                 datasets
+            }
+            let priceData = {
+                labels: priceLabels,
+                datasets: priceDatasets
             }
             let chartOptions = {
                 legend: {
@@ -114,35 +135,70 @@ function renderChart(lineChart, dataSrc) {
                 },
                 scales: {
                     xAxes: [{
+                        // stacked: false,
                         ticks: {
                             maxTicksLimit: 999999999,
-    
+
                         }
-                    }]
+                    }],
+                    // yAxes: [{
+                    //     stacked: true
+                    // }]
                 },
-                responsive: true
+                responsive: true,
+                maintainAspectRatio: true,
             };
             // console.log(lineChart);
-            lineChart = new Chart(speedCanvas, {
+            otherChart = new Chart(otherCanvas, {
                 type: 'line',
-                data: speedData,
+                data: otherData,
                 options: chartOptions
             })
-            lineChartInstance = lineChart
+            priceChart = new Chart(priceCanvas, {
+                type: 'line',
+                data: priceData,
+                options: chartOptions
+            })
+            otherChartInstance = otherChart
+            priceChartInstance = priceChart
             // console.log(lineChart);
         } else {
-            lineChart.data.labels.push(fromDateTime + " – " + toDateTime);
-            lineChart.data.datasets[0].data.push(avgPrice)
-            lineChart.data.datasets[2].data.push(avgQuantity)
-            lineChart.data.datasets[1].data.push(amountTrade)
-            lineChart.update();
+            if (otherChartInstance) {
+                otherChart.data.labels.push(fromDateTime + " – " + toDateTime);
+
+                otherChart.data.datasets[1].data.push(avgQuantity)
+
+                otherChart.data.datasets[0].data.push(amountTrade)
+
+
+                otherChart.data.labels.length > 20 && otherChart.data.labels.shift();
+                otherChart.data.datasets[1].data.length > 20 && otherChart.data.datasets[1].data.shift()
+                otherChart.data.datasets[0].data.length > 20 && otherChart.data.datasets[0].data.shift()
+                otherChart.update();
+            }
+            if (priceChartInstance) {
+                priceChart.data.labels.push(fromDateTime + " – " + toDateTime);
+                priceChart.data.labels.length > 20 && priceChart.data.labels.shift();
+                priceChart.data.datasets[0].data.push(avgPrice)
+                priceChart.data.datasets[0].data.length > 20 && priceChart.data.datasets[0].data.shift()
+                priceChart.update();
+            }
+
+
         }
     }
 
-    
+
 
 }
 
 
-setInterval(func, (5 * 1000));
+setInterval(func, (4 * 1000));
+setInterval(function (params) {
+    let speedPriceOnMinute = (+JSON.parse(dataSource[dataSource.length - 1]).p - beginPrice) * 60
+    beginPrice = +JSON.parse(dataSource[dataSource.length - 1]).p
+    document.getElementById("vprice").innerHTML = speedPriceOnMinute
+    document.getElementById("vprice").style.color = speedPriceOnMinute < 0 ? "red" : "green"
+
+}, 1000)
 
